1ac9f4c963aceb099e92a59a8a2751cd833a6735
Julien <cefadrom1@gmail.com>
2023-02-23 12:05:53 +0100

    feat: revert changed grades

--- src/lib/components/GradeCategoryTable.svelte
+++ src/lib/components/GradeCategoryTable.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { onMount } from 'svelte';
     import { calculateAverage, gradesWithCoefficientToList, validationLevel } from '../utils/grades.js';
     import GradeSelector from './GradeSelector.svelte';
 
@@ -8,13 +9,33 @@
     let categoryAverage, categoryLevel;
     $: categoryAverage = calculateAverage(gradesWithCoefficientToList(grades));
     $: categoryLevel = validationLevel(categoryAverage);
+
+    let initialGrades = [];
+    onMount(() => {
+        initialGrades = grades.map(grade => grade.grade);
+    });
+
+    let gradesChanged;
+    $: gradesChanged = grades.some((grade, index) => grade.grade !== initialGrades[index]);
+
+    function handleGradesRevert() {
+        grades = grades.map((grade, index) => ({
+            ...grade,
+            grade: initialGrades[index],
+        }));
+    }
 </script>
 
 
 <div class="flex flex-col divide-y divide-gray-400 mt-4 rounded-md bg-indigo-50 shadow-md">
 
-    <div class="p-1.5">
-        {category}
+    <div class="flex justify-between">
+        <div class="p-1.5">
+            {category}
+        </div>
+        <img src="icons/refresh.svg" alt="*" class="w-9 h-9 cursor-pointer p-1.5"
+             class:hidden={!gradesChanged}
+             on:click={handleGradesRevert}/>
     </div>
 
     <div class="flex divide-x divide-gray-400">
