20276591c096ab495cec63bbb0f267cdd4e4977e
Julien W <50249422+julien-wff@users.noreply.github.com>
2024-07-17 15:56:22 +0200

    feat: app config for port and env

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,14 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/julien-wff/cesi-dossier-synthese/internal/router"
+	"github.com/julien-wff/cesi-dossier-synthese/internal/utils"
 	"net/http"
 )
 
 func main() {
+	cfg := utils.GetAppConfig()
+
 	r := router.NewRouter()
 
-	fmt.Println("Starting server on port 8080")
-	if err := http.ListenAndServe(":8080", r); err != nil {
+	if cfg.Production {
+		fmt.Println("Starting production server on port", cfg.Port)
+	} else {
+		fmt.Println("Starting development server on port", cfg.Port)
+	}
+
+	if err := http.ListenAndServe(":"+cfg.Port, r); err != nil {
 		panic(err)
 	}
 }
