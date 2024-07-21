a8c34bfb2f4b373cdc8575e09993a4e5420c47cb
Julien W <50249422+julien-wff@users.noreply.github.com>
2024-07-21 18:08:53 +0200

    fix: correct app.html path

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './index.html',
+        './src/app.html',
         './src/**/*.svelte',
     ],
     theme: {
