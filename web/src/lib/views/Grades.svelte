d7b883b93e9899be05aba26f1c30b6658bfd911a
Julien W <cefadrom1@gmail.com>
2025-01-19 13:12:48 +0100

    fix: background not taking all screen space

--- web/src/lib/views/Grades.svelte
+++ web/src/lib/views/Grades.svelte
@@ -11,7 +11,7 @@
 </script>
 
 
-<div class="flex flex-col-reverse lg:flex-row items-center lg:items-start justify-center gap-4 md:gap-8 py-4 sm:p-4 md:p-8">
+<div class="min-h-screen flex flex-col-reverse lg:flex-row items-center lg:items-start justify-center gap-4 md:gap-8 py-4 sm:p-4 md:p-8">
     <div class="sm:max-w-xl w-full p-2 sm:p-4 bg-slate-100 dark:bg-slate-800 sm:shadow-md sm:rounded-md">
         {#each content as { name: group, categories }, i}
             <h1 class="mb-4 text-xl" class:mt-6={i !== 0}>{group}</h1>
