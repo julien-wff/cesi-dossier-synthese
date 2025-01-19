23251b39fb26cb93235d6f2918b1350f18f19eca
Julien W <cefadrom1@gmail.com>
2025-01-19 13:29:13 +0100

    feat: add missing dark mode spots

--- web/src/lib/views/Failure.svelte
+++ web/src/lib/views/Failure.svelte
@@ -9,7 +9,7 @@
 
 
 <div class="h-screen grid place-items-center">
-    <div class="bg-indigo-50 p-8 rounded-md shadow-md w-96">
+    <div class="bg-slate-100 dark:bg-slate-800 p-8 rounded-md shadow-md w-96">
         <h1 class="text-xl mb-6">Oh non ! Une erreur est survenue :(</h1>
         <p class="mb-6">{error}</p>
         <button class="bg-red-500 hover:bg-red-700 w-full text-white px-4 py-2 rounded-md"
