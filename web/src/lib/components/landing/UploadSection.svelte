b6831e6f0011ee8992ec99e18472f5d272e3c59c
Julien W <cefadrom1@gmail.com>
2025-01-21 19:16:51 +0100

    fix: prevent boxes from touching

--- web/src/lib/components/landing/UploadSection.svelte
+++ web/src/lib/components/landing/UploadSection.svelte
@@ -7,7 +7,7 @@
 </script>
 
 
-<section class="flex flex-col items-center justify-center min-h-svh p-2 lg:p-4 lg:pb-8">
+<section class="flex flex-col items-center justify-center min-h-svh p-2 lg:p-4 lg:pb-8 gap-4">
     <div class="w-full max-w-[64rem] flex-1 flex flex-col lg:flex-row justify-center lg:justify-between items-center gap-6">
         <h1 class="text-4xl lg:text-5xl font-bold text-center lg:text-left">
             Un outil pour visualiser ton <br class="hidden lg:inline"/>
