84811f77a9f9dbaacf87d3d9d8f4acc1f3f466ed
Julien W <cefadrom1@gmail.com>
2025-01-21 19:14:59 +0100

    feat: demo section

--- web/src/lib/views/Home.svelte
+++ web/src/lib/views/Home.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
     import UploadSection from '$lib/components/landing/UploadSection.svelte';
+    import DemoSection from '$lib/components/landing/DemoSection.svelte';
 </script>
 
-<UploadSection/>
+<div class="bg-slate-200 dark:bg-slate-900 flex flex-col gap-16">
+    <UploadSection/>
+    <DemoSection/>
+</div>
