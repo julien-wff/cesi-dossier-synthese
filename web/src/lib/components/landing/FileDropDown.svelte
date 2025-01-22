712920cb284cfa8241b9b726697c722b60223c43
Julien W <cefadrom1@gmail.com>
2025-01-22 14:21:23 +0100

    feat: indicate offline state

--- web/src/lib/components/landing/FileDropDown.svelte
+++ web/src/lib/components/landing/FileDropDown.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
     import FileText from 'lucide-svelte/icons/file-text';
+    import CloudAlert from 'lucide-svelte/icons/cloud-alert';
     import { appState, State } from '$lib/state/app.svelte.js';
+    import { browser } from '$app/environment';
 
     let inputField = $state<HTMLInputElement>();
     let drag = $state(false);
+    let online = $state(browser ? navigator.onLine : true);
 
     const getFieldFile = () => inputField?.files?.[0] ?? null;
 
@@ -26,11 +29,15 @@
 </script>
 
 
+<svelte:window ononline={() => (online = true)} onoffline={() => (online = false)}/>
+
 <div class="border-4 border-blue-500 dark:border-blue-400 rounded-2xl relative shadow-lg transition-colors"
      class:bg-slate-100={!drag}
      class:dark:bg-transparent={!drag}
      class:bg-blue-100={drag}
-     class:dark:bg-blue-900={drag}>
+     class:dark:bg-blue-900={drag}
+     class:border-red-500={!online}
+     class:dark:border-red-400={!online}>
 
     <input type="file"
            ondragenter={() => (drag = true)}
@@ -38,13 +45,20 @@
            ondrop={() => (drag = false)}
            onchange={handleChange}
            bind:this={inputField}
-           disabled={appState.state === State.Loading}
+           disabled={appState.state === State.Loading || !online}
+           class:cursor-pointer={appState.state !== State.Loading && online}
            class:cursor-progress={appState.state === State.Loading}
+           class:cursor-not-allowed={!online}
            accept="application/pdf"
-           class="w-80 h-48 opacity-0 cursor-pointer">
+           class="w-80 h-48 opacity-0">
 
     <div class="absolute inset-0 flex flex-col items-center justify-center gap-4 p-4 text-center pointer-events-none">
-        <FileText class="w-8 h-8"/>
-        <p class="font-bold">Clique ou dépose ton<br>dossier PDF ici</p>
+        {#if online}
+            <FileText class="w-8 h-8"/>
+            <p class="font-bold">Clique ou dépose ton<br>dossier PDF ici</p>
+        {:else}
+            <CloudAlert class="w-8 h-8"/>
+            <p class="font-bold">Hors ligne</p>
+        {/if}
     </div>
 </div>
