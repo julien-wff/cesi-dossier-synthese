77e2b2075fec1d49b48b95dc5f19614cea5cdb22
Julien W <cefadrom1@gmail.com>
2025-06-20 16:20:36 +0200

    feat: optimize font loading by hosting them

---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import inter400 from '@fontsource/inter/files/inter-latin-400-normal.woff2';
+    import inter700 from '@fontsource/inter/files/inter-latin-700-normal.woff2';
+    import type { Snippet } from 'svelte';
+
+    interface Props {
+        children: Snippet;
+    }
+
+    let { children }: Props = $props();
+</script>
+
+<svelte:head>
+    <link as="font" href={inter400} rel="preload" type="font/woff2">
+    <link as="font" href={inter700} rel="preload" type="font/woff2">
+</svelte:head>
+
+{@render children()}
