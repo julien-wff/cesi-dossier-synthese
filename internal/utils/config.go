cbb8d4da4694f600c77bd9d5a8e721c6f68700dd
Julien W <cefadrom1@gmail.com>
2025-07-08 21:20:32 +0200

    feat: env variable to toggle on proxy header

--- internal/utils/config.go
+++ internal/utils/config.go
@@ -11,11 +11,13 @@ type AppConfig struct {
 	Port              string
 	TelemetryUser     string
 	TelemetryPassword string
+	ProxyHeaders      bool
 }
 
 func GetAppConfig() *AppConfig {
 	env := strings.TrimSpace(strings.ToLower(os.Getenv("APP_ENV")))
 	port := os.Getenv("APP_PORT")
+	proxyHeaders := strings.TrimSpace(strings.ToLower(os.Getenv("PROXY_HEADERS")))
 
 	if p, err := strconv.Atoi(port); err != nil || p < 1 || p > 65535 {
 		port = "8080"
@@ -26,5 +28,6 @@ func GetAppConfig() *AppConfig {
 		Port:              port,
 		TelemetryUser:     os.Getenv("TELEMETRY_USER"),
 		TelemetryPassword: os.Getenv("TELEMETRY_PASSWORD"),
+		ProxyHeaders:      proxyHeaders == "true" || proxyHeaders == "1",
 	}
 }
