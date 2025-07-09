2dc96611d654ea27e86949ec9330e55a5b998c04
Julien W <cefadrom1@gmail.com>
2025-07-09 15:01:05 +0200

    chores: use snippet to avoid code duplication

--- web/src/lib/components/telemetry/TelemetryParsesList.svelte
+++ web/src/lib/components/telemetry/TelemetryParsesList.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { telemetryState } from '$lib/state/telemetry.svelte.js';
+    import { telemetryState, type TelemetryUnit } from '$lib/state/telemetry.svelte.js';
     import TelemetryParse from '$lib/components/telemetry/TelemetryParse.svelte';
 
     let successfulParses = $derived(telemetryState.telemetry.filter(t => t.success));
@@ -7,36 +7,26 @@
 </script>
 
 
+{#snippet parsesList(title: string, telemetry: TelemetryUnit[])}
+    <div class="h-fit flex items-center justify-center flex-col bg-slate-100 dark:bg-slate-700 p-2 sm:p-4 rounded-sm shadow-sm">
+        <span class="text-center font-bold mb-2 sm:mb-4">{title}</span>
+
+        <div class="flex flex-col gap-2 w-full">
+            {#if telemetry.length === 0}
+                <div class="text-sm text-gray-500 dark:text-gray-400 w-full text-center">
+                    No telemetry data available
+                </div>
+            {/if}
+
+            {#each telemetry.slice(-25).toReversed() as unit}
+                <TelemetryParse {unit}/>
+            {/each}
+        </div>
+    </div>
+{/snippet}
+
+
 <div class="grid sm:gap-4 gap-2 2xl:grid-cols-2">
-    <div class="h-fit flex items-center justify-center flex-col bg-slate-100 dark:bg-slate-700 p-2 sm:p-4 rounded-sm shadow-sm">
-        <span class="text-center font-bold mb-2 sm:mb-4">Latest successful parses</span>
-
-        <div class="flex flex-col gap-2 w-full">
-            {#if successfulParses.length === 0}
-                <div class="text-sm text-gray-500 dark:text-gray-400 w-full text-center">
-                    No telemetry data available
-                </div>
-            {/if}
-
-            {#each successfulParses.slice(-25).toReversed() as telemetry}
-                <TelemetryParse unit={telemetry}/>
-            {/each}
-        </div>
-    </div>
-
-    <div class="h-fit flex items-center justify-center flex-col bg-slate-100 dark:bg-slate-700 p-2 sm:p-4 rounded-sm shadow-sm">
-        <span class="text-center font-bold mb-2 sm:mb-4">Latest failed parses</span>
-
-        <div class="flex flex-col gap-2 w-full">
-            {#if failedParses.length === 0}
-                <div class="text-sm text-gray-500 dark:text-gray-400 w-full text-center">
-                    No telemetry data available
-                </div>
-            {/if}
-
-            {#each failedParses.slice(-25).toReversed() as telemetry}
-                <TelemetryParse unit={telemetry}/>
-            {/each}
-        </div>
-    </div>
+    {@render parsesList('Latest successful parses', successfulParses)}
+    {@render parsesList('Latest failed parses', failedParses)}
 </div>
