848c307f9a56b352353ba4f359cb050172be8f27
Julien W <cefadrom1@gmail.com>
2025-07-09 22:05:04 +0200

    feat: responsive telemetry page

--- web/src/lib/components/telemetry/TelemetryTimingStatsCard.svelte
+++ web/src/lib/components/telemetry/TelemetryTimingStatsCard.svelte
@@ -47,7 +47,7 @@
 <div class="flex items-center justify-center flex-col bg-slate-100 dark:bg-slate-700 p-2 sm:p-4 rounded-sm shadow-sm">
     <div class="text-center font-bold mb-2 md:mb-4">Timing Statistics</div>
 
-    <div class="flex flex-1 w-full gap-0.5">
+    <div class="flex flex-col md:flex-row flex-1 w-full gap-0.5">
         {#if timingKeys.length === 0}
             <div class="text-sm text-gray-500 dark:text-gray-400 w-full text-center">
                 No timing data available
@@ -56,8 +56,9 @@
 
         {#each timingKeys as { name, description }, idx (name)}
             {@const duration = `${accumulatedStatsAverage[name].toFixed(2)}ms`}
+            {@const fraction = (accumulatedStatsAverage[name] / totalAverageDuration * 100).toFixed(2)}
 
-            <div class="text-sm py-4 text-center"
+            <div class="text-sm py-4 text-center md:w-(--fraction) h-(--fraction) md:h-auto flex md:flex-col gap-1 items-center justify-center overflow-clip"
                  class:bg-red-300={idx === 0}
                  class:bg-orange-300={idx === 1}
                  class:bg-yellow-300={idx === 2}
@@ -75,8 +76,8 @@
                  class:dark:bg-pink-500={idx === 6}
                  class:dark:bg-gray-500={idx >= 7}
                  title="{description} ({duration})"
-                 style="width: {(accumulatedStatsAverage[name] / totalAverageDuration * 100).toFixed(2)}%">
-                <div class="line-clamp-1 mb-1">{description}</div>
+                 style="--fraction: {fraction}%">
+                <div class="line-clamp-1">{description}</div>
                 <div class="line-clamp-1">({duration})</div>
             </div>
         {/each}
