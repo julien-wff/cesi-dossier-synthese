1aac1d774b3aa77c4a0ce284dfde0208451c61df
Julien W <cefadrom1@gmail.com>
2025-07-09 14:57:22 +0200

    feat: split latest parses by success

--- web/src/lib/components/telemetry/TelemetryParsesList.svelte
+++ web/src/lib/components/telemetry/TelemetryParsesList.svelte
@@ -1,21 +1,42 @@
 <script lang="ts">
     import { telemetryState } from '$lib/state/telemetry.svelte.js';
     import TelemetryParse from '$lib/components/telemetry/TelemetryParse.svelte';
+
+    let successfulParses = $derived(telemetryState.telemetry.filter(t => t.success));
+    let failedParses = $derived(telemetryState.telemetry.filter(t => !t.success));
 </script>
 
 
-<div class="flex items-center justify-center flex-col bg-slate-100 dark:bg-slate-700 p-2 sm:p-4 rounded-sm shadow-sm">
-    <span class="text-center font-bold mb-2 sm:mb-4">Latest parses</span>
+<div class="grid sm:gap-4 gap-2 2xl:grid-cols-2">
+    <div class="h-fit flex items-center justify-center flex-col bg-slate-100 dark:bg-slate-700 p-2 sm:p-4 rounded-sm shadow-sm">
+        <span class="text-center font-bold mb-2 sm:mb-4">Latest successful parses</span>
 
-    <div class="flex flex-col gap-2 w-full">
-        {#if telemetryState.telemetry.length === 0}
-            <div class="text-sm text-gray-500 dark:text-gray-400 w-full text-center">
-                No telemetry data available
-            </div>
-        {/if}
+        <div class="flex flex-col gap-2 w-full">
+            {#if successfulParses.length === 0}
+                <div class="text-sm text-gray-500 dark:text-gray-400 w-full text-center">
+                    No telemetry data available
+                </div>
+            {/if}
 
-        {#each telemetryState.telemetry.slice(-25).toReversed() as telemetry}
-            <TelemetryParse unit={telemetry}/>
-        {/each}
+            {#each successfulParses.slice(-25).toReversed() as telemetry}
+                <TelemetryParse unit={telemetry}/>
+            {/each}
+        </div>
+    </div>
+
+    <div class="h-fit flex items-center justify-center flex-col bg-slate-100 dark:bg-slate-700 p-2 sm:p-4 rounded-sm shadow-sm">
+        <span class="text-center font-bold mb-2 sm:mb-4">Latest failed parses</span>
+
+        <div class="flex flex-col gap-2 w-full">
+            {#if failedParses.length === 0}
+                <div class="text-sm text-gray-500 dark:text-gray-400 w-full text-center">
+                    No telemetry data available
+                </div>
+            {/if}
+
+            {#each failedParses.slice(-25).toReversed() as telemetry}
+                <TelemetryParse unit={telemetry}/>
+            {/each}
+        </div>
     </div>
 </div>
